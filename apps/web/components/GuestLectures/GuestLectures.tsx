b2bb7fcec303963b00ccf858c7e872d9ae4f1627
Zolo-Ryan <utkarshswaroopsri187@gmail.com>
2023-11-23 20:05:12 +0530

    Made GL responsive

--- apps/web/components/GuestLectures/GuestLectures.tsx
+++ apps/web/components/GuestLectures/GuestLectures.tsx
@@ -33,9 +33,9 @@ const GuestLectures = () => {
 
   return (
     <div className='w-full h-4/5 flex flex-col items-center justify-between'>
-      <div className='text-7xl'>Guest Lectures</div>
+      <div className='text-4xl sm:text-5xl md:text-7xl xl:text-9xl'>Guest Lectures</div>
 
-      <div className='h-4/6 w-4/5 flex overflow-x-auto gap-2 px-2' ref={scrollRef}>
+      <div className='h-4/6 w-4/5 flex overflow-x-auto gap-2 px-2 gCardResponsive' ref={scrollRef}>
         <GLcard forwardedRef={cardRef} />
         <GLcard forwardedRef={cardRef} />
         <GLcard forwardedRef={cardRef} />
@@ -44,9 +44,9 @@ const GuestLectures = () => {
         <GLcard forwardedRef={cardRef} />
       </div>
 
-      <div className='w-1/5 flex justify-evenly'>
-        <div className='border-2 w-2/5 text-center py-3 text-xl border-b-8 border-blue-500 rounded-tl-2xl cursor-pointer' onClick={scrollPrev}>Prev</div>
-        <div className='border-2 w-2/5 text-center py-3 text-xl border-b-8 border-blue-500 rounded-tr-2xl cursor-pointer' onClick={scrollNext}>Next</div>
+      <div className='w-1/2 pt-2 flex justify-evenly'>
+        <div className='border-2 w-full md:w-2/5 sm:w-1/2 text-center py-3 text-xl border-b-4 border-blue-500 rounded-tl-2xl cursor-pointer mr-2' onClick={scrollPrev}>Prev</div>
+        <div className='border-2 w-full md:w-2/5 sm:w-1/2 text-center py-3 text-xl border-b-4 border-blue-500 rounded-tr-2xl cursor-pointer' onClick={scrollNext}>Next</div>
       </div>
     </div>
   )
