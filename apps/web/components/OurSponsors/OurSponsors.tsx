53fbcee49ef980513324783ef52b65382c3f0366
KhushmeetChugh <133222681+KhushmeetChugh@users.noreply.github.com>
2023-11-24 16:20:47 +0530

    i have made change in layout.css,ourSponsers,sponcersCard to make sponcy section responsive

--- apps/web/components/OurSponsors/OurSponsors.tsx
+++ apps/web/components/OurSponsors/OurSponsors.tsx
@@ -3,21 +3,35 @@ import SponsorCategory from './SponsorCategory'
 
 const OurSponsors = () => {
   return (
-    <div className='w-full h-full flex flex-col items-center justify-evenly'>
-      <div className='text-5xl'>Our Sponsors</div>
+    <div className='w-full h-full flex flex-col items-center justify-evenly' style={{marginTop:-10}}>
+      <div className='text-5xl lg:text-4xl'>Our Sponsors</div>
+      <style jsx>{`
+  @media (max-width: 310px) {
+    .text-5xl {
+      font-size: 3rem; /* Adjust the font size for 310px */
+    }
+    .lg\:text-4xl {
+      font-size: 2.5rem; /* Adjust the font size for 310px for lg breakpoint */
+    }
+  }
+`}</style>
 
-      <div className='h-4/6 w-4/5 flex justify-center flex-wrap gap-y-2'>
+      <div className='flex justify-center flex-wrap gap-y-4' style={{}}>
+        <div className=" flex justify-center flex-wrap gap-y-4">
+
         <SponsorCategory category='Title Sponsors' sponsors={8} />
         <SponsorCategory category='Coding Partners' sponsors={4} />
         <SponsorCategory category='Fashion Partners' sponsors={3} />
-        <SponsorCategory category='Hackathon Partners' sponsors={2} />
+        <SponsorCategory category='Hackathon Partners' sponsors={3} />
         <SponsorCategory category='Travel Partners' sponsors={1} />
+        </div>
+
+      <div className='flex flex-col' style={{}}>
+        <div className='text-center text-xl'>Want to Sponsor?</div>
+        <div className='border-2 text-center p-3 text-xl border-b-8 border-blue-500 rounded-tl-2xl cursor-pointer' style={{}}>Contact Us</div>
+      </div>
       </div>
 
-      <div className='flex flex-col items-center gap-2'>
-        <div className='text-center text-xl'>Want to Sponsor?</div>
-        <div className='border-2 text-center p-3 text-xl border-b-8 border-blue-500 rounded-tl-2xl cursor-pointer'>Contact Us</div>
-      </div>
     </div>
   )
 }
