75dd31d4c5629aaa9ecd4357042640a67151b929
KhushmeetChugh <133222681+KhushmeetChugh@users.noreply.github.com>
2023-11-25 15:30:19 +0530

    responsiveness
    responsiveness of sponsor section updated

--- apps/web/components/OurSponsors/SponsorCategory.tsx
+++ apps/web/components/OurSponsors/SponsorCategory.tsx
@@ -6,7 +6,18 @@ const SponsorCategory = ({ category, sponsors }: { category: string, sponsors: n
 
   return (
     <div className={`flex flex-col items-center gap-2 ${(sponsors > 2) ? 'w-full' : 'w-1/2'} category`}>
-      <div className='text-2xl'>{category}</div>
+      <div className='text-2xl lg:text-1xl'>{category}</div>
+      <style jsx>{`
+  @media (max-width: 320px) {
+    .text-2xl {
+      font-size: 2rem; /* Adjust the font size for 310px */
+    }
+    .lg\:text-1xl {
+      font-size: 1.1rem; /* Adjust the font size for 310px for lg breakpoint */
+    }
+  }
+`}</style>
+      
       <div className='flex justify-evenly w-5/6 flex-wrap h-fit gap-3'>
         {
           nums.map((i) => {
