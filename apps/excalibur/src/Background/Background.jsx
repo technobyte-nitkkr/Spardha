176ee5144804f0dc3c09cb4295a706f0e88269b2
Debatreyta <debatreyadas@gmail.com>
2023-12-14 09:38:51 +0530

    Excalibur made Responsive

--- apps/excalibur/src/Background/Background.jsx
+++ apps/excalibur/src/Background/Background.jsx
@@ -2,7 +2,7 @@ import './Background.css'
 export default function Background(){
     return(
         <div className="background_graphics">
-            <img src="./public/images/excalibur_background_png.png" alt="" />
+            <img src="/images/excalibur_background_png.png" alt="" />
         </div>
     )
 }
