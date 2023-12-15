bd7cd196f46fd1ef4f906da4990ba5fee0cb9ed7
sanyam <sanyam9922@gmail.com>
2023-12-15 12:16:59 +0530

    responsive-sponsor-section

--- apps/web/components/OurSponsors/SponsorCard.tsx
+++ apps/web/components/OurSponsors/SponsorCard.tsx
@@ -9,7 +9,7 @@ interface SponsorData{
 }
 const SponsorCard = ({ len,sponsor }: { len: number,sponsor: SponsorData }): JSX.Element => {
   return (
-    <div className={`${((len > 2) ? 'w-1/5' : 'w-2/5')} h-12 bg-white border-2 border-blue-300 rounded-3xl`}>
+    <div className={`${((len > 2) ? 'w-1/5' : 'w-5/5')} h-12 bg-white border-2 border-blue-300 rounded-3xl`}>
       <Link className='h-full w-1/6' href={sponsor.targetUrl} target='_blank'>
         <Image alt='abstract' className='p-1 h-full w-full rounded-3xl' height={1000} src={sponsor.imageUrl} width={1000} />
       </Link>
