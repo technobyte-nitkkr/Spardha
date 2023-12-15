a0360cb9bbca5f5ceae4852cb6749ece84076ffa
Zolo Ryan <119104324+Zolo-Ryan@users.noreply.github.com>
2023-12-15 11:34:25 +0530

    [UPDATE] added animation to event categories section and make it dynamic as well (#42)
    * Made GL responsive
    
    * responsiveness finalize;
    
    * fetched data from backend and added links to instagram facebook and linkedin in glCards
    
    * fixed guest lecture scrolling by vansh sukhija sir
    
    * added key to glCards to avoid warning messages in console,added ? in cardRef.current?.offsetWidth! else causing an error due to value undefined
    
    * .
    
    * added sponsors from backend and added their target urls
    
    * Sponsors added and build issues resolved
    
    * Added animation to event categories and make the section dynamic also
    
    * corrected animation timing

--- apps/web/components/Events/Events.tsx
+++ apps/web/components/Events/Events.tsx
@@ -1,22 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import EventsCard from "./EventsCard";
 
-const myImage = "/assets/abstractimage.jpg";
+interface CategoriesElement {
+  categoryName: string,
+  imgUrl: string,
+  icon: string,
+}
+function startAnimation(i: number, cards: NodeListOf<HTMLLabelElement>) {
+  let currentIndex = i;
+  setInterval(() => {
+    removePreviousCardStyle(cards, currentIndex - 1 % cards.length);
+    setCurrentCardStyle(cards, currentIndex % cards.length);
+    setNextCardStyle(cards, currentIndex % cards.length);
+    setNextNextCardStyle(cards, currentIndex % cards.length);
+    currentIndex = (currentIndex + 1) % cards.length;
+    // console.log(currentIndex)
+  }, 4000);
+  return currentIndex;
+}
+function setNextNextCardStyle(cards: NodeListOf<HTMLLabelElement>, currentIndex: number) {
+  if (cards.length <= 2) return;
+  let i = currentIndex;
+  if (i === cards.length - 1) i = 1;
+  if (i === cards.length - 2) i = 0;
+  cards[i + 2].style.display = "block";
+  cards[i + 2].style.transform = "translatex(30%) scale(0.6)";
+  cards[i + 2].style.opacity = "0.2";
+  cards[i + 2].style.zIndex = "0";
+}
+function setNextCardStyle(cards: NodeListOf<HTMLLabelElement>, currentIndex: number) {
+  if (cards.length <= 1) return;
+  let i = currentIndex;
+  if (i === cards.length - 1) i = -1;
+  cards[i + 1].style.display = "block";
+  cards[i + 1].style.transform = "translatex(15%) scale(0.8)";
+  cards[i + 1].style.opacity = "0.4";
+  cards[i + 1].style.zIndex = "0";
+}
+function setCurrentCardStyle(cards: NodeListOf<HTMLLabelElement>, i: number) {
+  cards[i].style.display = "block";
+  cards[i].style.transform = "translatex(0) scale(1)";
+  cards[i].style.opacity = "1";
+  cards[i].style.zIndex = "1";
+}
+function removePreviousCardStyle(cards: NodeListOf<HTMLLabelElement>, currentIndex: number) {
+  if (cards.length <= 1) return;
+  let i = currentIndex;
+  if (i < 0) i = cards.length - 1;
+  cards[i].style.transform = "translatex(30%) scale(0.0)";
+  cards[i].style.opacity = "0.2";
+  cards[i].style.zIndex = "0";
+}
 
 const Events: React.FC = () => {
+  const [categories, setCategories] = useState<CategoriesElement[]>([]);
+  let currentIndex = 0;
+  let cards: NodeListOf<HTMLLabelElement>;
+
+  useEffect(() => {
+    cards = document.querySelectorAll(".card");
+    currentIndex = startAnimation(currentIndex, cards);
+  },[categories])
+
+  useEffect(() => {
+    fetch("https://us-central1-techspardha-87928.cloudfunctions.net/api2/events/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data: { message: string, success: boolean, data:{ categories: CategoriesElement[]}}) => {
+        setCategories(data.data.categories);
+      })
+      .catch((err: Error) => err);
+  },[])
+
   return (
     <div className="w-full h-full flex flex-col items-center justify-evenly">
       <div className="text-5xl sm:text-5xl md:text-6xl xl:text-5xl font-starlord-1">
         Events Categories
       </div>
-
-      <div className="h-4/6 w-1/2 flex overflow-x-auto snap-x snap-mandatory">
-        <EventsCard image={myImage} />
-        <EventsCard image={myImage} />
-        <EventsCard image={myImage} />
-        <EventsCard image={myImage} />
-        <EventsCard image={myImage} />
-        <EventsCard image={myImage} />
+      <div className="h-4/6 lg:w-1/2 w-4/5 flex relative">
+        {categories.map((category,i) => {
+          return(
+            <EventsCard image={category.imgUrl} key={i}/>
+          )
+        })}
       </div>
       <div className="w-1/4 pt-2 flex justify-evenly">
         <div className="border-2 w-full md:w-2/5 sm:w-1/2 text-center py-3 text-xl border-b-8 border-blue-500 rounded-tl-2xl cursor-pointer mr-2">
