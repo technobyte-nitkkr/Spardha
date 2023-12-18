00949ea93a8435daf74be1564c3134974a14dd20
Debatreyta <debatreyadas@gmail.com>
2023-12-18 20:30:15 +0530

    Conflicts resolving , now excalibur has countdown and a timeline, registration button removed temporarilly

--- apps/excalibur/src/App.jsx
+++ apps/excalibur/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Background from './Background/Background'
+import Countdown from './Countdown/Countdown'
 import Navbar from './navbar/Navbar'
 import MyTypeform from './Typeform/MyTypeform'
+import TimeLine from './Timeline/Timeline'
 function App() {
   return (
     <div className='excalibur'>
       <Navbar />
       <h1>EXCALIBUR</h1>
-      <MyTypeform />
+      {/* <MyTypeform /> */}
+      <Countdown />
+      <TimeLine />
       <Background />
     </div>
   )
