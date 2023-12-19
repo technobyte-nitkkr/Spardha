0e16a42486c0701731446732178799eede8d19d6
Debatreyta <debatreyadas@gmail.com>
2023-12-19 07:41:30 +0530

    Lock file conflict kr rha tha, delete kr diya bc

--- apps/excalibur/src/App.jsx
+++ apps/excalibur/src/App.jsx
@@ -3,6 +3,7 @@ import Background from './Background/Background'
 import Countdown from './Countdown/Countdown'
 import Navbar from './navbar/Navbar'
 import MyTypeform from './Typeform/MyTypeform'
+import Event from './Event/Event'
 import TimeLine from './Timeline/Timeline'
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <h1>EXCALIBUR</h1>
       {/* <MyTypeform /> */}
       <Countdown />
+      <Event />
       <TimeLine />
       <Background />
     </div>
