db1b5c5c248fcd6d27ecec799193e64b946b907b
Debatreya Das <116421305+Debatreya@users.noreply.github.com>
2023-12-22 14:55:59 +0530

    Excalibur Website complete ❤️ (#55)
    * Conflicts resolving , now excalibur has countdown and a timeline, registration button removed temporarilly
    
    * Lock file conflict kr rha tha, delete kr diya bc
    
    * Status till 21st dec 5pm
    
    * Excalibur complete

--- apps/excalibur/src/Typeform/MyTypeform.jsx
+++ apps/excalibur/src/Typeform/MyTypeform.jsx
@@ -2,26 +2,27 @@ import './MyTypeform.css'
 
 import React, { useEffect } from 'react';
 
-const MyTypeform = () => {
+export default function Typeform(){
   useEffect(() => {
+    // Create a script element
     const script = document.createElement('script');
-    script.src = 'https://embed.typeform.com/next/embed.js';
+    
+    // Set the script source to the Typeform embed script
+    script.src = '//embed.typeform.com/next/embed.js';
     script.async = true;
 
-    const div = document.createElement('div');
-    div.className = "typeform_button"
-    div.setAttribute('data-tf-live', '01HHRM3NH9FG268WFQ9BYBM1Y0');
-
-    document.body.appendChild(div);
+    // Append the script to the document body
     document.body.appendChild(script);
 
+    // Cleanup function to remove the script on component unmount
     return () => {
-      document.body.removeChild(div);
       document.body.removeChild(script);
     };
   }, []);
-
-  return <div />;
-};
-
-export default MyTypeform;
+  return (
+    <>
+      <div className='typeform_button' data-tf-live="01HHRM3NH9FG268WFQ9BYBM1Y0"></div>
+      {/* <script src="//embed.typeform.com/next/embed.js"></script> */}
+    </>
+  )
+}
