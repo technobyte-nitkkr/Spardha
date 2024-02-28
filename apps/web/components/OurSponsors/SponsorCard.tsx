3762dcb2f7778e19b345884cee40e8d298d56a7e
Deepak Kumar <dk10112002@gmail.com>
2024-02-28 13:11:05 +0530

    added alt attribute in sponsors

--- apps/web/components/OurSponsors/SponsorCard.tsx
+++ apps/web/components/OurSponsors/SponsorCard.tsx
@@ -6,12 +6,13 @@ interface SponsorData{
   imageUrl: string,
   name: string,
   targetUrl: string,
+  alt?: string
 }
 const SponsorCard = ({sponsor}: { sponsor: SponsorData}): JSX.Element => {
   return (
     <div className={`h-7 md:h-11 sm:w-2/5 sm:h-14 w-full bg-white border-2 border-blue-300 rounded-3xl`}>
       <Link className='h-full w-1/6' href={sponsor.targetUrl || ""} target='_blank'>
-        <Image alt='abstract' className='p-1 h-full  rounded-3xl object-contain' height={1000} src={sponsor.imageUrl} width={1000} />
+        <Image alt={sponsor.alt || ""} className='p-1 h-full  rounded-3xl object-contain' height={1000} src={sponsor.imageUrl} width={1000} />
       </Link>
     </div>
   )
